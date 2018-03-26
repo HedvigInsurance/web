344042869f2ee9fd76be23ade7b8c4a53e7f6722
Philip Harrison <philip@mailharrison.com>
2018-03-26 14:40:46 +0200

    Fix test deploy when on master

--- scripts/test-deploy.sh
+++ scripts/test-deploy.sh
@@ -2,12 +2,12 @@
 set -uex
 
 if [ -z "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-    if [ "${TRAVIS_BRANCH}" == "develop" ]; then
+    if [ "${TRAVIS_BRANCH}" == "master" ]; then
         yarn build
         aws s3 sync build/ ${TEST_S3_WEBSITE_BUCKET}
     else
-        echo "Not on develop, will not deploy to test"
+        echo "Not on master, will not deploy to test"
     fi
 else
-    echo "Not on develop, will not deploy to test"
+    echo "Not on master, will not deploy to test"
 fi
