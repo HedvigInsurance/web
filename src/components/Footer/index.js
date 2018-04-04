9867d8bf38d73c4f026e852268c0cd744594ae38
Philip Harrison <philip@mailharrison.com>
2018-04-04 11:30:38 +0200

    Removing duplicate app download links from footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import Lazyload from 'react-lazyload';
 
 import './footer.css';
 
@@ -23,36 +22,6 @@ const Footer = () => (
             </Link>
           </nav>
         </div>
-      </div>
-      <div className="Footer__end">
-        <a
-          href="https://itunes.apple.com/se/app/hedvig/id1303668531?mt=8"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="Footer__appLink"
-        >
-          <Lazyload height={46} offset={200}>
-            <img
-              src="/assets/web/appstores/app-store-badge@2x.png"
-              alt="Ladda ner på App Store"
-              height={54}
-            />
-          </Lazyload>
-        </a>
-        <a
-          href="https://play.google.com/store/apps/details?id=com.hedvig.app"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="Footer__appLink"
-        >
-          <Lazyload height={46} offset={200}>
-            <img
-              src="/assets/web/appstores/google-play-badge@2x.png"
-              alt="Ladda ner på Google Play"
-              height={54}
-            />
-          </Lazyload>
-        </a>
       </div>
     </div>
   </footer>
