9a83a7523bbdfb8fdd356a9fc5a0b47761a6718c
Philip Harrison <philip@mailharrison.com>
2018-05-02 21:57:48 +0200

    Format css and js in entire project

--- src/pages/terms/index.js
+++ src/pages/terms/index.js
@@ -12,21 +12,25 @@ const Terms = () => (
       <div className="pure-u-1-1 pure-u-md-3-5">
         <section>
           <p>
-            Här hittar du våra olika försäkringsvillkor. Där i står det vad din försäkring gäller
-            för. Vi har gjort vårt allra bästa för att de ska vara enkla att läsa, men har du några
-            frågor är det bara att kontakta Hedvig direkt via din app eller maila
-            <a href="mailto:hedvig@hedvig.com"> hedvig@hedvig.com</a>. I appen hittar du en tydlig
-            översikt över allt din försäkring omfattar, och hur den gäller för olika saker.
+            Här hittar du våra olika försäkringsvillkor. Där i står det vad din
+            försäkring gäller för. Vi har gjort vårt allra bästa för att de ska
+            vara enkla att läsa, men har du några frågor är det bara att
+            kontakta Hedvig direkt via din app eller maila
+            <a href="mailto:hedvig@hedvig.com"> hedvig@hedvig.com</a>. I appen
+            hittar du en tydlig översikt över allt din försäkring omfattar, och
+            hur den gäller för olika saker.
           </p>
           <p>
-            Det är viktigt att du läser villkoret och ditt försäkringsbrev. Skador ersätts alltid
-            enligt det villkor som gäller när en skada inträffar.
+            Det är viktigt att du läser villkoret och ditt försäkringsbrev.
+            Skador ersätts alltid enligt det villkor som gäller när en skada
+            inträffar.
           </p>
           <p>
-            Här nedanför hittar du också något som heter förköpsinformation. Det är en
-            sammanfattning av vad försäkringen täcker. Där i finns också några viktiga
-            begränsningar. Förköpsinformation är sådan information som du har rätt att få innan du
-            köper en försäkring. Informationen finns så klart tillgänglig i appen också.
+            Här nedanför hittar du också något som heter förköpsinformation. Det
+            är en sammanfattning av vad försäkringen täcker. Där i finns också
+            några viktiga begränsningar. Förköpsinformation är sådan information
+            som du har rätt att få innan du köper en försäkring. Informationen
+            finns så klart tillgänglig i appen också.
           </p>
         </section>
         <section className="pure-g">
@@ -57,7 +61,8 @@ const Terms = () => (
                 <a href="https://s3.eu-central-1.amazonaws.com/com-hedvig-web-content/F%C3%B6rs%C3%A4kringsvillkor+-+Andrahandshyrare+(Februari+2018).pdf">
                   För dig som hyr bostadsrätt i andra hand
                 </a>{' '}
-                och vill att din hyresvärds saker och lägenhet ska täckas av försäkringen
+                och vill att din hyresvärds saker och lägenhet ska täckas av
+                försäkringen
               </span>
             </p>
           </div>
@@ -90,7 +95,8 @@ const Terms = () => (
                 <a href="https://s3.eu-central-1.amazonaws.com/com-hedvig-web-content/F%C3%B6rk%C3%B6psinformation+-+Andrahandshyrare+(Februari+2018).pdf">
                   För dig som hyr bostadsrätt i andra hand
                 </a>{' '}
-                och vill att din hyresvärds saker och lägenhet ska täckas av försäkringen
+                och vill att din hyresvärds saker och lägenhet ska täckas av
+                försäkringen
               </span>
             </p>
           </div>
