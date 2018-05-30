ed396a6bd44f61616ddbc3922f2a82bf506773e0
Philip Harrison <philip@mailharrison.com>
2018-05-30 13:54:35 +0200

    Add send sms to download app for desktop visitors

--- src/components/AppLink/index.js
+++ src/components/AppLink/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Cookies from 'js-cookie';
 
+import { utmParamsToBranchLinkOptions } from 'src/services/utm-to-branch';
+
 const { NODE_ENV = 'development' } = process.env;
 
 // Dynamically creating a link automatically copies over
@@ -12,8 +14,8 @@ class AppLink extends React.Component {
     children: PropTypes.node.isRequired,
     channel: PropTypes.string,
     campaign: PropTypes.string,
-    tags: PropTypes.string,
-    keywords: PropTypes.string,
+    tags: PropTypes.arrayOf(PropTypes.string),
+    keywords: PropTypes.arrayOf(PropTypes.string),
     feature: PropTypes.string,
     stage: PropTypes.string,
   };
@@ -43,49 +45,8 @@ class AppLink extends React.Component {
   componentDidMount() {
     if (!window.branch || typeof window.branch.link !== 'function') return;
 
-    // utm-params get set in plugins/utm-params
-    // utm params are passed in from ads (fb, adwords, twitter)
-    // Object structure:
-    // https://github.com/segmentio/utm-params
-    // {
-    //   "source": "google",
-    //   "medium": "medium",
-    //   "term": "keyword",
-    //   "content": "some content",
-    //   "name": "some campaign"
-    // }
     const utmParams = Cookies.getJSON('utm-params') || {};
-
-    // Branch param to UTM param
-    // https://support.branch.io/support/solutions/articles/6000127549-utm-parameters-and-the-branch-dashboard
-    const mapBranchToUtmParams = {
-      channel: 'source',
-      feature: 'medium',
-      campaign: 'name',
-      tags: 'content',
-      keywords: 'term',
-    };
-
-    const linkOptions = [
-      'channel',
-      'campaign',
-      'tags',
-      'feature',
-      'keywords',
-      'stage',
-    ].reduce((acc, key) => {
-      const value = this.props[key];
-      const utmValue = utmParams[mapBranchToUtmParams[key]];
-      // utm param values always take precedent over static values
-      // This enables ad attribution in app
-      const linkValue = utmValue || value;
-
-      if (linkValue) {
-        acc[key] = linkValue;
-      }
-
-      return acc;
-    }, {});
+    const linkOptions = utmParamsToBranchLinkOptions(utmParams, this.props);
 
     const path =
       this.context && this.context.location && this.context.location.pathname;
