08c507caf2d5cee5f768e798ce973715e3a23cad
Oscar Nylander <oscar@hedvig.com>
2018-06-15 17:21:23 +0200

    debug template

--- src/cms/preview-templates/landing-page-preview.js
+++ src/cms/preview-templates/landing-page-preview.js
@@ -2,19 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { LandingTemplate } from 'src/templates/landing-page';
 
-const LandingPagePreview = ({ entry }) => (
-  <LandingTemplate
-    section1={entry.getIn(['data', 'section1'])}
-    section2={entry.getIn(['data', 'section2'])}
-    section3={entry.getIn(['data', 'section3'])}
-    section4={entry.getIn(['data', 'section4'])}
-    section5={entry.getIn(['data', 'section5'])}
-    section6={entry.getIn(['data', 'section6'])}
-    section7={entry.getIn(['data', 'section7'])}
-    section8={entry.getIn(['data', 'section8'])}
-    section9={entry.getIn(['data', 'section9'])}
-  />
-);
+const LandingPagePreview = ({ entry }) => {
+  console.log(entry.getIn(['data']));
+  return (
+    <LandingTemplate
+      section1={entry.getIn(['data', 'section1'])}
+      section2={entry.getIn(['data', 'section2'])}
+      section3={entry.getIn(['data', 'section3'])}
+      section4={entry.getIn(['data', 'section4'])}
+      section5={entry.getIn(['data', 'section5'])}
+      section6={entry.getIn(['data', 'section6'])}
+      section7={entry.getIn(['data', 'section7'])}
+      section8={entry.getIn(['data', 'section8'])}
+      section9={entry.getIn(['data', 'section9'])}
+    />
+  );
+};
 
 LandingPagePreview.propTypes = {
   entry: PropTypes.shape({
