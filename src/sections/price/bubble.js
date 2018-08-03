87bf404dd7242cfd647418d1874b24e458f3f63f
Sam Pettersson <sam@sampettersson.com>
2018-08-03 16:44:12 +0200

    Refactor to make the code a bit nicer

---
+++ src/sections/price/bubble.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'react-emotion';
+
+const Container = styled('div')`
+  margin: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Bubble = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 65px;
+  height: 130px;
+  width: 130px;
+  box-shadow: 1px 3px 3px rgba(0, 0, 0, 0.2);
+  font-size: 25px;
+  flex-direction: column;
+`;
+
+const Title = styled('span')`
+  margin-bottom: 15px;
+  font-size: 22px;
+`;
+
+const PriceValue = styled('span')`
+  font-size: 30px;
+  color: white;
+`;
+
+const AroundLabel = styled('span')`
+  color: white;
+  font-size: 15px;
+`;
+
+export const PriceBubble = ({ title, aroundLabel, monthlyLabel, price }) => (
+  <Container>
+    <Title className="u-fontWeightBold">{title}</Title>
+    <Bubble className="u-backgroundPrimaryPurple">
+      <AroundLabel>{aroundLabel}</AroundLabel>
+      <PriceValue className="u-fontWeightBold">{price}</PriceValue>
+      <MonthLabel>{monthlyLabel}</MonthLabel>
+    </Bubble>
+  </Container>
+);
---
+++ src/sections/price/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from 'react-emotion';
+import PropTypes from 'prop-types';
+
+import { PriceBubble } from './bubble';
+
+const Container = styled('div')`
+  padding: 50px 0;
+`;
+
+const Title = styled('div')`
+  text-align: center;
+  margin-bottom: 30px;
+`;
+
+const PriceBubbles = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+`;
+
+const Subline = styled('div')`
+  margin-top: 25px;
+  text-align: center;
+`;
+
+export const PriceSection = ({
+  heading,
+  bottomParagraph,
+  rentalPrice,
+  rentalTitle,
+  ownedPrice,
+  ownedTitle,
+  monthlyLabel,
+  aroundLabel,
+}) => (
+  <div className="u-backgroundWhite">
+    <Container>
+      <Title>
+        <h2>{heading}</h2>
+      </Title>
+      <PriceBubbles>
+        <PriceBubble
+          title={rentalTitle}
+          price={rentalPrice}
+          monthlyLabel={monthlyLabel}
+          aroundLabel={aroundLabel}
+        />
+        <PriceBubble
+          title={ownedTitle}
+          price={ownedPrice}
+          monthlyLabel={monthlyLabel}
+          aroundLabel={aroundLabel}
+        />
+      </PriceBubbles>
+      <Subline className="u-fontSize85">
+        <span>{bottomParagraph}</span>
+      </Subline>
+    </Container>
+  </div>
+);
+
+PriceSection.propTypes = {
+  heading: PropTypes.string.isRequired,
+  bottomParagraph: PropTypes.string.isRequired,
+  rentalPrice: PropTypes.string.isRequired,
+  rentalTitle: PropTypes.string.isRequired,
+  ownedPrice: PropTypes.string.isRequired,
+  ownedTitle: PropTypes.string.isRequired,
+  monthlyLabel: PropTypes.string.isRequired,
+  aroundLabel: PropTypes.string.isRequired,
+};
