0d308ebad44d278d25f526b023de1d9d4388474e
Oscar Nylander <oscar@hedvig.com>
2018-08-21 13:14:26 +0200

    sticky container

--- src/cms/preview-templates/header-preview.js
+++ src/cms/preview-templates/header-preview.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'constate';
+import { StickyContainer } from 'react-sticky';
 
 import Header from 'src/components/Header';
 
 const HeaderPreview = ({ entry }) => (
   <Provider initialState={{}}>
-    <Header
-      data={{
-        links: entry.getIn(['data', 'links']).toJS(),
-        ctaText: entry.getIn(['data', 'ctaText']),
-      }}
-    />
+    <StickyContainer>
+      <Header
+        data={{
+          links: entry.getIn(['data', 'links']).toJS(),
+          ctaText: entry.getIn(['data', 'ctaText']),
+        }}
+      />
+    </StickyContainer>
   </Provider>
 );
 
