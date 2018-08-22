e013574056e95b9d8d2144851db593bc6c66a8bd
Oscar Nylander <oscar@hedvig.com>
2018-08-22 15:47:28 +0200

    general blog page prototype

---
+++ src/pages/blog/index.js
@@ -0,0 +1,71 @@
+/* eslint-disable */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { StickyContainer } from 'react-sticky';
+import remark from 'remark';
+import reactRenderer from 'remark-react';
+
+import Header, { headerPropTypes } from 'src/components/Header';
+import Footer, { footerPropTypes } from 'src/components/Footer';
+
+const Blog = ({ data }) => {
+  const { posts, page, header, footer } = data;
+  return (
+    <main className="Site">
+      <Helmet>
+        <title>{page.title}</title>
+      </Helmet>
+      <StickyContainer>
+        <Header data={header} />
+        <div className="Site-content">
+          {posts.edges.map(({ node: { frontmatter } }) => (
+            <article>
+              <h2>{frontmatter.title}</h2>
+              <img src={frontmatter.topImage} alt="" />
+              <div>
+                {
+                  remark()
+                    .use(reactRenderer)
+                    .processSync(frontmatter.content).contents
+                }
+              </div>
+            </article>
+          ))}
+        </div>
+        <Footer data={footer} />
+      </StickyContainer>
+    </main>
+  );
+};
+
+export const blogQuery = graphql`
+  query BlogQuery {
+    posts: allMarkdownRemark(filter: { id: { regex: "/blog/" } }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+            topImage
+            content
+          }
+        }
+      }
+    }
+
+    page: dataYaml(id: { regex: "/blog/" }) {
+      title
+    }
+
+    header: dataYaml(id: { regex: "/header/" }) {
+      ...Header_data
+    }
+
+    footer: dataYaml(id: { regex: "/footer/" }) {
+      ...Footer_data
+    }
+  }
+`;
+
+export default Blog;
