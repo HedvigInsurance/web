aa4adb7e6ef0d8015d11b8059e0028b495fb5ed0
Oscar Nylander <oscar@hedvig.com>
2018-08-23 16:39:33 +0200

    fix previews for header and footer

--- src/cms/preview-templates/footer-preview.js
+++ src/cms/preview-templates/footer-preview.js
@@ -3,19 +3,22 @@ import PropTypes from 'prop-types';
 import Footer from 'src/components/Footer';
 
 const FooterPreview = ({ entry }) => (
-  <Footer
-    data={{
-      linkSection1: entry.getIn(['data', 'linkSection1']).toJS(),
-      linkSection2: entry.getIn(['data', 'linkSection2']).toJS(),
-      appStoreAlt: entry.getIn(['data', 'appStoreAlt']),
-      playStoreAlt: entry.getIn(['data', 'playStoreAlt']),
-      facebookAlt: entry.getIn(['data', 'facebookAlt']),
-      instagramAlt: entry.getIn(['data', 'instagramAlt']),
-      twitterAlt: entry.getIn(['data', 'twitterAlt']),
-      copyrightText: entry.getIn(['data', 'copyrightText']),
-      legalText: entry.getIn(['data', 'legalText']),
-    }}
-  />
+  <React.Fragment>
+    <Footer
+      data={{
+        se: entry.getIn(['data', 'se']).toJS(),
+        en: entry.getIn(['data', 'en']).toJS(),
+      }}
+      langKey="se"
+    />
+    <Footer
+      data={{
+        se: entry.getIn(['data', 'se']).toJS(),
+        en: entry.getIn(['data', 'en']).toJS(),
+      }}
+      langKey="en"
+    />
+  </React.Fragment>
 );
 
 FooterPreview.propTypes = {
--- src/cms/preview-templates/header-preview.js
+++ src/cms/preview-templates/header-preview.js
@@ -6,16 +6,30 @@ import { StickyContainer } from 'react-sticky';
 import Header from 'src/components/Header';
 
 const HeaderPreview = ({ entry }) => (
-  <Provider initialState={{}}>
-    <StickyContainer>
-      <Header
-        data={{
-          links: entry.getIn(['data', 'links']).toJS(),
-          ctaText: entry.getIn(['data', 'ctaText']),
-        }}
-      />
-    </StickyContainer>
-  </Provider>
+  <React.Fragment>
+    <Provider initialState={{}}>
+      <StickyContainer>
+        <Header
+          data={{
+            se: entry.getIn(['data', 'se']).toJS(),
+            en: entry.getIn(['data', 'en']).toJS(),
+          }}
+          langKey="se"
+        />
+      </StickyContainer>
+    </Provider>
+    <Provider initialState={{}}>
+      <StickyContainer>
+        <Header
+          data={{
+            se: entry.getIn(['data', 'se']).toJS(),
+            en: entry.getIn(['data', 'en']).toJS(),
+          }}
+          langKey="en"
+        />
+      </StickyContainer>
+    </Provider>
+  </React.Fragment>
 );
 
 HeaderPreview.propTypes = {
