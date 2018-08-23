eeb1eab61afc018a233ff373920f4ee5b5319e18
Oscar Nylander <oscar@hedvig.com>
2018-08-23 16:14:42 +0200

    Push it a few pixels away

--- src/components/Header/cta-link-container.js
+++ src/components/Header/cta-link-container.js
@@ -13,7 +13,7 @@ const PositionAnimation = styled('div')`
 
   @media (min-width: 960px) {
     margin-right: 0;
-    transform: translateX(150px);
+    transform: translateX(170px);
     ${({ isVisible }) => isVisible && `transform: translateX(0);`};
   }
 `;
