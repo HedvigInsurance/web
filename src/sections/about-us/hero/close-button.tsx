c7ffdb2c6c72c800c171cf79c1fa1a84406fb430
Sam Pettersson <sam@sampettersson.com>
2018-09-05 11:36:56 +0200

    Hero WIP

---
+++ src/sections/about-us/hero/close-button.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import styled from "react-emotion"
+import { Spring } from "react-spring"
+
+const Button = styled("button")({
+    position: "absolute",
+    top: 20,
+    left: 20,
+    color: "white"
+})
+
+interface CloseButtonProps {
+    onClick: () => void,
+    hidden: boolean
+}
+
+export const CloseButton: React.SFC<CloseButtonProps> = ({ onClick, hidden }) =>
+    <Spring from={{ opacity: hidden ? 1 : 0 }} to={{ opacity: hidden ? 0 : 1 }}>
+        {styles => <Button style={styles} onClick={onClick}>Stoppa</Button>}
+    </Spring>
