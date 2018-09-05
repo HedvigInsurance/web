6e49da9ad66d650b0ed10aee05ecb5c0b9a79810
Sam Pettersson <sam@sampettersson.com>
2018-09-05 14:44:31 +0200

    Formatting

--- src/sections/about-us/hedvigers/desktop/index.test.tsx
+++ src/sections/about-us/hedvigers/desktop/index.test.tsx
@@ -4,19 +4,19 @@ import renderer from 'react-test-renderer';
 import { Desktop } from './';
 
 it('renders correctly', () => {
-    const teamtailorUsers = [
-        {
-            name: 'mock',
-            title: 'mock',
-            picture: {
-                large: 'https://mockimage.com',
-            },
-        },
-    ];
+  const teamtailorUsers = [
+    {
+      name: 'mock',
+      title: 'mock',
+      picture: {
+        large: 'https://mockimage.com',
+      },
+    },
+  ];
 
-    const tree = renderer
-        .create(<Desktop teamtailorUsers={teamtailorUsers} />)
-        .toJSON();
+  const tree = renderer
+    .create(<Desktop teamtailorUsers={teamtailorUsers} />)
+    .toJSON();
 
-    expect(tree).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();
 });
--- src/sections/about-us/hero/index.tsx
+++ src/sections/about-us/hero/index.tsx
@@ -1,87 +1,106 @@
-import * as React from "react"
-import styled from "react-emotion"
-import { Container, ActionMap } from "constate"
+import * as React from 'react';
+import styled from 'react-emotion';
+import { Container, ActionMap } from 'constate';
 
-import { Title } from "./title"
-import { Player } from "./player"
-import { CloseButton } from "./close-button";
+import { Title } from './title';
+import { Player } from './player';
+import { CloseButton } from './close-button';
 
-const HeroContainer = styled("div")({
-    width: "100%",
-    overflow: "hidden",
-    position: "relative"
-})
+const HeroContainer = styled('div')({
+  width: '100%',
+  overflow: 'hidden',
+  position: 'relative',
+});
 
 interface ShadowProps {
-    hidden: boolean
+  hidden: boolean;
 }
 
-const Shadow = styled("div")({
-    position: "absolute",
-    width: "100%",
-    height: "100%",
+const Shadow = styled('div')(
+  {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
     top: 0,
     left: 0,
-    backgroundColor: "rgba(0,0,0,0.7)",
+    backgroundColor: 'rgba(0,0,0,0.7)',
     padding: 20,
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    transition: "opacity 1000ms"
-}, ({ hidden }: ShadowProps) => ({
-    opacity: hidden ? 0 : 1
-}))
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    transition: 'opacity 1000ms',
+  },
+  ({ hidden }: ShadowProps) => ({
+    opacity: hidden ? 0 : 1,
+  }),
+);
 
 interface State {
-    isFullScreen: boolean,
-    videoRef: React.RefObject<HTMLVideoElement>
+  isFullScreen: boolean;
+  videoRef: React.RefObject<HTMLVideoElement>;
 }
 
 interface Actions {
-    setFullScreen: (isFullScreen: boolean) => void,
-    setVideoRef: (videoRef: React.RefObject<HTMLVideoElement>) => void
+  setFullScreen: (isFullScreen: boolean) => void;
+  setVideoRef: (videoRef: React.RefObject<HTMLVideoElement>) => void;
 }
 
 const actions: ActionMap<State, Actions> = {
-    setFullScreen: isFullScreen => () => ({
-        isFullScreen
-    }),
-    setVideoRef: videoRef => () => ({
-        videoRef
-    })
-}
+  setFullScreen: (isFullScreen) => () => ({
+    isFullScreen,
+  }),
+  setVideoRef: (videoRef) => () => ({
+    videoRef,
+  }),
+};
 
-const handleVideoRef = setVideoRef => ref => {
-    setVideoRef({
-        onFullscreen: () => {
-            ref.webkitEnterFullscreen();
-        }
-    })
-}
+const handleVideoRef = (setVideoRef) => (ref) => {
+  setVideoRef({
+    onFullscreen: () => {
+      ref.webkitEnterFullscreen();
+    },
+  });
+};
 
-const onPlay = ({ videoRef, setFullScreen }: { videoRef: React.RefObject<HTMLVideoElement>, setFullScreen: (isFullScreen: boolean) => void }) => () => {
-    if (videoRef.current && videoRef.current.webkitEnterFullscreen) {
-        const video = videoRef.current
-        video.muted = false
-        video.pause()
-        video.currentTime = 0
-        video.play()
-        video.webkitEnterFullscreen()
-        return
-    }
+const onPlay = ({
+  videoRef,
+  setFullScreen,
+}: {
+  videoRef: React.RefObject<HTMLVideoElement>;
+  setFullScreen: (isFullScreen: boolean) => void;
+}) => () => {
+  if (videoRef.current && videoRef.current.webkitEnterFullscreen) {
+    const video = videoRef.current;
+    video.muted = false;
+    video.pause();
+    video.currentTime = 0;
+    video.play();
+    video.webkitEnterFullscreen();
+    return;
+  }
 
-    setFullScreen(true)
-}
+  setFullScreen(true);
+};
 
-export const Hero = () =>
-    <Container actions={actions} initialState={{ isFullScreen: false, videoRef: React.createRef<HTMLVideoElement>() }}>
-        {({ isFullScreen, setFullScreen, videoRef }) => (
-            <HeroContainer>
-                <Player isFullScreen={isFullScreen} videoRef={videoRef} />
-                <Shadow hidden={isFullScreen}>
-                    <Title clickedPlayButton={onPlay({ videoRef, setFullScreen })} />
-                </Shadow>
-                <CloseButton onClick={() => setFullScreen(false)} hidden={!isFullScreen} />
-            </HeroContainer>
-        )}
-    </Container>
+export const Hero = () => (
+  <Container
+    actions={actions}
+    initialState={{
+      isFullScreen: false,
+      videoRef: React.createRef<HTMLVideoElement>(),
+    }}
+  >
+    {({ isFullScreen, setFullScreen, videoRef }) => (
+      <HeroContainer>
+        <Player isFullScreen={isFullScreen} videoRef={videoRef} />
+        <Shadow hidden={isFullScreen}>
+          <Title clickedPlayButton={onPlay({ videoRef, setFullScreen })} />
+        </Shadow>
+        <CloseButton
+          onClick={() => setFullScreen(false)}
+          hidden={!isFullScreen}
+        />
+      </HeroContainer>
+    )}
+  </Container>
+);
