331dfd686a1e33dc3f652180a796f92c89317359
Sam Pettersson <sam@sampettersson.com>
2018-09-10 21:50:02 +0200

    Add state to css injector

--- src/cms/css-injector.tsx
+++ src/cms/css-injector.tsx
@@ -1,19 +1,40 @@
 import * as React from 'react';
 import { renderToString } from 'react-dom/server';
 import { renderStylesToString } from 'emotion-server';
+import { Container, ActionMap } from 'constate';
 
-let hasInjectedCSS = false;
+interface State {
+  hasInjectedCSS: boolean;
+}
+
+interface Actions {
+  setHasInjectedCSS: (hasInjectedCSS: boolean) => State;
+}
+
+const actions: ActionMap<State, Actions> = {
+  setHasInjectedCSS: (hasInjectedCSS) => ({
+    hasInjectedCSS,
+  }),
+};
+
+const initialState: State = {
+  hasInjectedCSS: false,
+};
 
 export const CSSInjector: React.SFC = ({ children }) => (
-  <div
-    ref={(ref) => {
-      if (ref && !hasInjectedCSS) {
-        hasInjectedCSS = true;
-        const css = renderStylesToString(renderToString(children));
-        ref.ownerDocument.head.innerHTML += css;
-      }
-    }}
-  >
-    {children}
-  </div>
+  <Container actions={actions} initialState={initialState}>
+    {({ hasInjectedCSS, setHasInjectedCSS }) => (
+      <div
+        ref={(ref) => {
+          if (ref && !hasInjectedCSS) {
+            setHasInjectedCSS(true);
+            const css = renderStylesToString(renderToString(children));
+            ref.ownerDocument.head.innerHTML += css;
+          }
+        }}
+      >
+        {children}
+      </div>
+    )}
+  </Container>
 );
