0e3687517272673bf7935c39e9e4d6c2e31736b5
Sam Pettersson <sam@sampettersson.com>
2018-09-10 21:24:02 +0200

    Don’t inject CSS every time

--- src/cms/css-injector.tsx
+++ src/cms/css-injector.tsx
@@ -2,10 +2,13 @@ import * as React from 'react';
 import { renderToString } from 'react-dom/server';
 import { renderStylesToString } from 'emotion-server';
 
+let hasInjectedCSS = false;
+
 export const CSSInjector: React.SFC = ({ children }) => (
   <div
     ref={(ref) => {
-      if (ref) {
+      if (ref && !hasInjectedCSS) {
+        hasInjectedCSS = true;
         const css = renderStylesToString(renderToString(children));
         ref.ownerDocument.head.innerHTML += css;
       }
