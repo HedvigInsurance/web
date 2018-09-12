53bf865cc8dd6b8194f71612413febaf3c648126
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-09-12 12:55:21 +0200

    Fix case sensitive import

--- src/cms/utils/markdown.test.tsx
+++ src/cms/utils/markdown.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { mount } from 'enzyme';
-import { Markdown } from './Markdown';
+import { Markdown } from './markdown';
 
 it('renders markdown like it should', () => {
   const markdown = `
