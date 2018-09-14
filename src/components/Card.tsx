d0ec63eba61a4fc3e591fbe5fde36b83bb98aa7a
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-09-14 16:07:29 +0200

    Fix looooong string

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,8 +4,11 @@ import { colors } from '@hedviginsurance/brand';
 const Card = styled('div')({
   backgroundColor: colors.WHITE,
   borderRadius: 16,
-  boxShadow:
-    '0 1px 5px 0 rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12);',
+  boxShadow: [
+    '0 1px 5px 0 rgba(0, 0, 0, 0.2)',
+    '0 2px 2px 0 rgba(0, 0, 0, 0.14)',
+    '0 3px 1px -2px rgba(0, 0, 0, 0.12)',
+  ].join(),
   overflow: 'hidden',
 });
 
