328d5b05c49e4276ceb854fb1bded79adb5be83d
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-09-14 09:56:23 +0200

    [wip] add a console log

--- src/cms/preview-templates/blog-post-preview.js
+++ src/cms/preview-templates/blog-post-preview.js
@@ -3,22 +3,37 @@ import { Provider } from 'constate';
 
 import { CSSInjector } from 'src/cms/css-injector';
 import { BlogPostTemplate } from 'src/templates/blog-post';
+import { StaticRouter } from 'react-router-dom';
 
-const BlogPostPreview = ({ entry }) => (
-  <CSSInjector>
-    <Provider initialState={{}}>
-      <BlogPostTemplate
-        title={entry.getIn('data', 'title')}
-        date={entry.getIn('data', 'date')}
-        author={entry.getIn('data', 'author')}
-        content={entry.getIn('data', 'content')}
-        topImage={entry.getIn('data', 'topImage')}
-        topImageColor={entry.getIn('data', 'topImageColor')}
-        tags={entry.getIn('data', 'tags').toJS()}
-        cta={entry.getIn('data', 'cta').toJS()}
-      />
-    </Provider>
-  </CSSInjector>
-);
+const BlogPostPreview = ({ entry }) => {
+  console.log(
+    typeof entry.getIn(['data', 'tags']),
+    entry.getIn(['data', 'tags']),
+  );
+  console.log(
+    typeof entry.getIn(['data', 'cta']),
+    entry.getIn(['data', 'cta']),
+  );
+  const tags = entry.getIn(['data', 'tags']);
+  const cta = entry.getIn(['data', 'cta']);
+  return (
+    <CSSInjector>
+      <Provider initialState={{}}>
+        <StaticRouter context={{ path: '/blog/fake-post' }}>
+          <BlogPostTemplate
+            title={entry.getIn(['data', 'title'])}
+            date={entry.getIn(['data', 'date'])}
+            author={entry.getIn(['data', 'author'])}
+            content={entry.getIn(['data', 'content'])}
+            topImage={entry.getIn(['data', 'topImage'])}
+            topImageColor={entry.getIn(['data', 'topImageColor'])}
+            tags={tags && tags.toJS ? tags.toJS() : tags}
+            cta={cta && cta.toJS()}
+          />
+        </StaticRouter>
+      </Provider>
+    </CSSInjector>
+  );
+};
 
 export default BlogPostPreview;
