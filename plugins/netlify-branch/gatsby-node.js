779473eaa5330bcb3235734251956dad4d72b147
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-09-17 13:51:40 +0200

    Fallback to deploy prime url

--- plugins/netlify-branch/gatsby-node.js
+++ plugins/netlify-branch/gatsby-node.js
@@ -4,7 +4,9 @@ exports.modifyWebpackConfig = ({ config }) => {
   config.merge({
     plugins: [
       new webpack.DefinePlugin({
-        URL: JSON.stringify(process.env.URL || ''),
+        URL: JSON.stringify(
+          process.env.URL || process.env.DEPLOY_PRIME_URL || '',
+        ),
         NETLIFY_BRANCH: JSON.stringify(process.env.BRANCH || 'local'),
       }),
     ],
