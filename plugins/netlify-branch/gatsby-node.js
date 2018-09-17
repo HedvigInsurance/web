bc055b2ad87cb34d90967f987f35065441851802
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-09-17 13:54:00 +0200

    Use process.env. for url

--- plugins/netlify-branch/gatsby-node.js
+++ plugins/netlify-branch/gatsby-node.js
@@ -4,7 +4,7 @@ exports.modifyWebpackConfig = ({ config }) => {
   config.merge({
     plugins: [
       new webpack.DefinePlugin({
-        URL: JSON.stringify(
+        'process.env.URL': JSON.stringify(
           process.env.URL || process.env.DEPLOY_PRIME_URL || '',
         ),
         NETLIFY_BRANCH: JSON.stringify(process.env.BRANCH || 'local'),
