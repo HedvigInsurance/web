08ddef80727eccd74ee96ae33229a09d3710819f
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-09-17 14:47:54 +0200

    Kill dead type definitions 🔪

--- typings/modules/remark-parse.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'remark-parse' {
-  import { UnifiedMiddleware } from 'unified';
-  const remarkParse: UnifiedMiddleware;
-  export default remarkParse;
-}
--- typings/modules/remark-react.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'remark-react' {
-  import { RemarkRenderer } from 'remark';
-  const func: () => RemarkRenderer;
-  export default func;
-}
