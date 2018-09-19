8320041969076a4086e04347bdd5f613faf0b7e2
Fredrik Holmdahl <holmdahl.fredrik@gmail.com>
2018-09-19 17:27:26 +0200

    AppLink, MediaQuery and removed styling

--- src/sections/landing/get-started.tsx
+++ src/sections/landing/get-started.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import styled from 'react-emotion';
 import { colors } from '@hedviginsurance/brand';
+import { fonts } from '@hedviginsurance/brand';
+import AppLink from 'src/components/AppLink';
+import MediaQuery from 'react-responsive';
 
 const Section = styled('div')({
   backgroundColor: colors.OFF_WHITE,
@@ -17,9 +20,6 @@ const Container = styled('div')({
     flexDirection: 'column-reverse',
     paddingTop: 50,
     paddingBottom: 50,
-  },
-  '@media (min-width: 601px) and (max-width: 959px)': {
-    paddingTop: 100,
   },
 });
 
@@ -53,12 +53,13 @@ const HeadlineContainer = styled('div')({
 
 const Headline = styled('h2')({
   minWidth: '100%',
+  fontFamily: fonts.SORAY,
   '@media (max-width: 600px)': {
     textAlign: 'center',
   },
 });
 
-const Paragrah = styled('p')({
+const Paragraph = styled('p')({
   paddingTop: 10,
   '@media (max-width: 600px)': {
     textAlign: 'center',
@@ -67,25 +68,17 @@ const Paragrah = styled('p')({
 });
 
 const GetStartedBtn = styled('div')({
-  display: 'block',
   paddingTop: 50,
-  '@media (max-width: 600px)': {
-    display: 'none',
-  },
 });
 
 const GetStartedBtnMobile = styled('div')({
-  display: 'none',
-  '@media (max-width: 600px)': {
-    paddingTop: 30,
-    display: 'flex',
-  },
+  paddingTop: 30,
 });
 
-const LinkTag = styled('a')({
+const LinkTag = styled(AppLink)({
   padding: '15px 75px',
   '@media (max-width: 600px)': {
-    linewidth: '100%',
+    width: '100%',
   },
 });
 
@@ -101,16 +94,16 @@ class GetStarted extends React.Component {
     return (
       <Section>
         <Container className="Container">
-          <GetStartedBtnMobile>
-            <LinkTag
-              tags="home-hero"
-              className="Button u-fontSize10 u-colorWhite u-backgroundPrimaryGreen u-md-spaceMR12 u-lg-spaceMR12 u-fontWeightBold"
-              feature="organic"
-              href="https://hedvig.test-app.link/fLAzSHSKkQ"
-            >
-              Kom igång
-            </LinkTag>
-          </GetStartedBtnMobile>
+          <MediaQuery query="(max-width: 600px)">
+            <GetStartedBtnMobile>
+              <LinkTag
+                tags="home-hero"
+                className="Button u-fontSize10 u-colorWhite u-backgroundPrimaryGreen u-md-spaceMR12 u-lg-spaceMR12 u-fontWeightBold"
+              >
+                Kom igång
+              </LinkTag>
+            </GetStartedBtnMobile>
+          </MediaQuery>
 
           <ImageContainer>
             <ImgTag src={'/assets/social/get-started-image.png'} />
@@ -121,17 +114,17 @@ class GetStarted extends React.Component {
               2 minuter. 8 frågor. <br />
               Din perfekta försäkring.
             </Headline>
-            <Paragrah>Ingen Bindningstid. Månadsvis betalning.</Paragrah>
-            <GetStartedBtn>
-              <LinkTag
-                tags="home-hero"
-                className="Button u-fontSize10 u-colorWhite u-backgroundPrimaryGreen u-md-spaceMR12 u-lg-spaceMR12 u-fontWeightBold"
-                feature="organic"
-                href="https://hedvig.test-app.link/fLAzSHSKkQ"
-              >
-                Kom igång
-              </LinkTag>
-            </GetStartedBtn>
+            <Paragraph>Ingen Bindningstid. Månadsvis betalning.</Paragraph>
+            <MediaQuery query="(min-width: 600px)">
+              <GetStartedBtn>
+                <LinkTag
+                  tags="home-hero"
+                  className="Button u-fontSize10 u-colorWhite u-backgroundPrimaryGreen u-md-spaceMR12 u-lg-spaceMR12 u-fontWeightBold"
+                >
+                  Kom igång
+                </LinkTag>
+              </GetStartedBtn>
+            </MediaQuery>
           </HeadlineContainer>
         </Container>
       </Section>
