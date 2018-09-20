bdcbee73a67c3dd508379ec2c2f7ed4576f09b53
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-09-20 15:49:30 +0200

    Add first iteration of breadcrumbs

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import styled from 'react-emotion';
+import GatsbyLink from 'gatsby-link';
+
+interface BreadcrumbProps {
+  to?: string;
+}
+
+const BreadcrumbComponent = styled('li')({
+  display: 'inline-block',
+  fontSize: 16,
+  '&:not(:first-of-type):before': {
+    content: '"\\203A"', // rsaquo
+    display: 'inline-block',
+    padding: '0 10px',
+  },
+});
+
+const PlainBreadcrumb = styled('span')({
+  fontStyle: 'italic',
+});
+
+const Breadcrumb: React.SFC<BreadcrumbProps> = ({ to, children }) => (
+  <BreadcrumbComponent>
+    {to ? (
+      <GatsbyLink to={to}>{children}</GatsbyLink>
+    ) : (
+      <PlainBreadcrumb>{children}</PlainBreadcrumb>
+    )}
+  </BreadcrumbComponent>
+);
+
+const Breadcrumbs = styled('ul')({
+  listStyle: 'none',
+  margin: 0,
+  padding: 0,
+});
+
+export { Breadcrumbs, Breadcrumb };
