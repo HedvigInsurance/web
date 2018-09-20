b5253d9b9af1f6027562d3714fc2b49639f86e34
Fredrik Holmdahl <holmdahl.fredrik@gmail.com>
2018-09-20 17:04:43 +0200

    Correct folder structure

--- src/sections/landing/get-started.tsx
+++ src/sections/landing/get-started.tsx
@@ -4,15 +4,12 @@ import { colors } from '@hedviginsurance/brand';
 import { fonts } from '@hedviginsurance/brand';
 import AppLink from 'src/components/AppLink';
 import MediaQuery from 'react-responsive';
-import Img from 'gatsby-image';
 
 interface Props {
   heading1: string;
   heading2: string;
   paragraph: string;
   buttontext: string;
-  imageAltText: string;
-  image?: any; // TODO type this better, too late tonight though
 }
 
 const Section = styled('div')({
@@ -37,6 +34,9 @@ const ImageContainer = styled('div')({
   marginRight: 20,
   maxWidth: '50%',
   position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
   '@media (max-width: 600px)': {
     marginRight: 0,
     maxWidth: '300px',
@@ -59,7 +59,6 @@ const HeadlineContainer = styled('div')({
 const Headline = styled('h2')({
   minWidth: '100%',
   fontFamily: fonts.SORAY,
-  fontWeight: 600,
   '@media (max-width: 600px)': {
     textAlign: 'center',
   },
@@ -88,8 +87,8 @@ const LinkTag = styled(AppLink)({
   },
 });
 
-const Image = styled(Img)({
-  width: 480,
+const ImgTag = styled('img')({
+  maxWidth: '80%',
   '@media (max-width: 959px)': {
     maxWidth: '100%',
   },
@@ -110,13 +109,11 @@ const GetStarted: React.SFC<Props> = (props) => (
       </MediaQuery>
 
       <ImageContainer>
-        {props.image && (
-          <Image sizes={props.image.image.sizes} alt={props.imageAltText} />
-        )}
+        <ImgTag src={'/assets/social/get-started-image.png'} />
       </ImageContainer>
 
       <HeadlineContainer>
-        <Headline className="u-md-fontSize4 u-lg-fontSize2">
+        <Headline className="u-md-fontSize4 u-lg-fontSize4 u-fontSize6">
           {props.heading1} <br />
           {props.heading2}
         </Headline>
