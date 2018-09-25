32768bc4633a984f6ee4fe2d4692822083e0aee2
Fredrik Holmdahl <holmdahl.fredrik@gmail.com>
2018-09-25 09:22:02 +0200

    tsx files included

---
+++ src/components/Animations/ClaimOnPhone.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { LottieLoader } from 'src/components/LottieLoader';
+
+const claimOnPhoneAnimation = require('assets/animations/three-explainers/claim-on-your-phone.json');
+
+const claimOnPhoneOptions = {
+  loop: true,
+  autoplay: false,
+  renderer: 'svg',
+  animationData: claimOnPhoneAnimation,
+  rendererSettings: {
+    progressiveLoad: true,
+    preserveAspectRatio: 'xMaxYMin meet',
+  },
+};
+
+const ClaimOnPhone = React.forwardRef(
+  ({ sideLength }: { sideLength: number }, ref) => (
+    <LottieLoader
+      ref={ref}
+      options={claimOnPhoneOptions}
+      width={sideLength}
+      height={sideLength}
+    />
+  ),
+);
+
+ClaimOnPhone.propTypes = {
+  ref: PropTypes.func.isRequired,
+  sideLength: PropTypes.number.isRequired,
+};
+
+export default ClaimOnPhone;
---
+++ src/components/Animations/PaidRightAway.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { LottieLoader } from 'src/components/LottieLoader';
+
+const paidRightAwayAnimation = require('assets/animations/three-explainers/paid-right-away.json');
+
+const paidRightAwayOptions = {
+  loop: false,
+  autoplay: false,
+  renderer: 'svg',
+  animationData: paidRightAwayAnimation,
+  rendererSettings: {
+    progressiveLoad: true,
+    preserveAspectRatio: 'xMaxYMin meet',
+  },
+};
+
+const PaidRightAway = React.forwardRef(
+  ({ sideLength }: { sideLength: number }, ref) => (
+    <LottieLoader
+      ref={ref}
+      options={paidRightAwayOptions}
+      width={sideLength}
+      height={sideLength}
+    />
+  ),
+);
+
+PaidRightAway.propTypes = {
+  ref: PropTypes.func.isRequired,
+  sideLength: PropTypes.number.isRequired,
+};
+
+export default PaidRightAway;
