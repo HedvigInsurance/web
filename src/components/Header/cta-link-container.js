c24080c83c9388119eeda1cd642609e9a5a879dc
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-11-27 13:00:44 +0100

    Hopefully fix the header cta once and for all

--- src/components/Header/cta-link-container.js
+++ src/components/Header/cta-link-container.js
@@ -1,33 +1,67 @@
 import React from 'react';
 import { Container } from 'constate';
-import styled from 'react-emotion';
+import styled, { css } from 'react-emotion';
 import PropTypes from 'prop-types';
 
 import AppLink from 'src/components/AppLink';
+import { Mount } from 'react-lifecycle-components';
 
-const PositionAnimation = styled('div')`
-  transition: transform 250ms;
-  transform: translateY(-150px);
+const PositionAnimation = styled('div')(
+  ({ animate, width, isVisible }) => `
+  ${animate && 'transition: transform 250ms;'}
   margin-right: 15px;
-  ${({ isVisible }) => isVisible && `transform: translateY(0);`};
+  transform: ${isVisible ? 'translateY(0)' : 'translateY(-150px)'};
 
   @media (min-width: 960px) {
     margin-right: 0;
-    transform: translateX(125px);
-    ${({ isVisible }) => isVisible && `transform: translateX(0);`};
+    transform: ${
+      isVisible
+        ? `translateX(-25px);`
+        : `translateX(${width ? width + 15 : -25}px)`
+    };
   }
-`;
+`,
+);
+const nowrap = css({
+  whiteSpace: 'nowrap',
+});
 
 export const CTALinkContainer = ({ ctaText, children }) => (
   <div className="u-md-inlineBlock u-lg-inlineBlock">
-    <Container context="CTAWaypoint" initialState={{}}>
-      {({ CTAWaypointReached }) => (
-        <PositionAnimation isVisible={CTAWaypointReached}>
+    <Container
+      context="CTAWaypoint"
+      initialState={{ buttonRef: React.createRef(), hasMounted: false }}
+      actions={{ mount: () => () => ({ hasMounted: true }) }}
+    >
+      {({ CTAWaypointReached, buttonRef, mount, hasMounted }) => (
+        <PositionAnimation
+          isVisible={CTAWaypointReached}
+          width={
+            (buttonRef && buttonRef.current && buttonRef.current.scrollWidth) ||
+            0
+          }
+          animate={hasMounted}
+        >
+          <Mount
+            on={() => {
+              setTimeout(() => {
+                mount();
+              }, 1);
+            }}
+          >
+            {null}
+          </Mount>
           <div>
             {children}
             <AppLink
               tags={['header']}
-              className="Button Header-cta-button u-colorWhite u-backgroundPrimaryGreen u-fontWeightBold"
+              className={`Button Header-cta-button u-colorWhite u-backgroundPrimaryGreen u-fontWeightBold ${nowrap}`}
+              innerRef={buttonRef}
+              style={{
+                position:
+                  buttonRef && buttonRef.current ? 'relative' : 'absolute',
+                left: buttonRef && buttonRef.current ? 0 : '100%',
+              }}
             >
               {ctaText}
             </AppLink>
